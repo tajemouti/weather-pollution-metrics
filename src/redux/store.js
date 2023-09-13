bf40e562d1b5521d9e723b5705d94b6c2086be04
tajemouti <tajemouti@gmail.com>
2023-09-13 11:32:49 +0100

    Import reducers to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import statesReducer from './states/statesSlice';
+import citiesReducer from './cities/citiesSlice';
+import cityReducer from './city/citySlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    states: statesReducer,
+    cities: citiesReducer,
+    city: cityReducer,
+  },
 });
 
 export default store;

57f7f16e1fb2c263ade4b529d31c94576046b4ad
tajemouti <tajemouti@gmail.com>
2023-09-14 00:06:13 +0100

    Secure API calls

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://api.airvisual.com/v2';
+const API_BASE_URL = 'https://api.airvisual.com/v2';
 const API_KEY = 'b2adf197-6bff-46b0-9a02-15ec2fe125c9';
 
 export { API_BASE_URL, API_KEY };

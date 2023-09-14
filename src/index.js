9e5ce0d55e72ad8aa326f1974f1c221d910bb075
tajemouti <tajemouti@gmail.com>
2023-09-14 09:53:26 +0100

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import store from './redux/store';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,5 +14,3 @@ root.render(
     </BrowserRouter>
   </Provider>,
 );
-
-reportWebVitals();
